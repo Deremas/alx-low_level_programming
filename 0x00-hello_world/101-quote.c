bbc3651223c8ed71a6c076e534522ce868609bac
Dereje Masresha <deremas27@gmail.com>
2023-07-16 04:17:10 -0700

    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

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,11 @@
 
 /**
  * main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19"
- * 
+ *
  * Return: Always 1.
  */
 int main(void)
 {
-	write("and that piece of art is useful" - Dora Korpar, 2015-10-19");
+	write("and that piece of art is useful\"  -  Dora Korpar, 2015-10-19\n");
 	return (1);
 }
