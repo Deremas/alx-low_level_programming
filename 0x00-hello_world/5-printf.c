1483a719149730a78c969aa8ed86b01e2031226b
Dereje Masresha <deremas27@gmail.com>
2023-07-16 02:17:08 -0700

    Write a C program that prints exactly with proper grammar,

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
-
-int main(void)
-/*
- *main - A program that print a line using printf function
+/**
+ * main - 5-printf.c 
+ * description - Write a C program that prints exactly with proper grammar,
  *
  * Return: Always 0 (Success)
  */
+
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 		return (0);
