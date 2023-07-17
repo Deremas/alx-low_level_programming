a5ab9a669507d2d61765394e84124dc91ee83e7e
Dereje Masresha <deremas27@gmail.com>
2023-07-16 21:26:45 -0700

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -2,13 +2,20 @@
 
 /**
  * main - Entry point
- * Description - 
+ * Description - list numbers fro 0 to 9
  *
  * Return: 0
  */
 
 int main(void)
 {
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		printf("%d", n);
+	}
+	printf("\n");
 
 	return (0);
 }
