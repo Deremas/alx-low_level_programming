648dfe6ee212545e18977aeee41ddb2cae0048f2
Dereje Masresha <deremas27@gmail.com>
2023-07-16 20:05:32 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description - print the alphabet in lowercase
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n = 97;
+	{
+		putchar(n);
+		n++;
+	}
+	return (0);
+}
+
