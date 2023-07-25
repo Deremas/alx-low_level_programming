7904981a0baf724cacb27bc729e0bca9351eba5f
Dereje Masresha <deremas27@gmail.com>
2023-07-24 18:52:33 -0700

    main.h

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98- using pointer to an int s parameter
+ * and updates the value of points to 98
+ * @n: input
+ * Return: n
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
