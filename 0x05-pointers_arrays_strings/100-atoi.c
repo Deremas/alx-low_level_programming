47c6a293a81b75c9cb1c1739e6d9f66a213f2ff5
Dereje Masresha <deremas27@gmail.com>
2023-07-25 17:40:40 -0700

    100

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,37 +9,30 @@
 int _atoi(char *s)
 
 {
-int a, b, c, len, f, digit;
+	int a = 0;
+	int b = 0;
+	int c = 0;
+	int d = 0;
 
-a = 0;
-b = 0;
-c = 0;
-len = 0;
-f = 0;
-digit = 0;
-
-while (s[len] != '\0')
-len++;
-while (a < len && f == 0)
-{
-if (s[a] == ' ')
-++b;
-
-if (s[a] >= '0' && s[a] <= '0')
-{
-digit = s[a] - '0';
-if (b % 2)
-digit = digit;
-c = c * 10 + digit;
-f = 1;
-if (s[a + 1] < '0' || s[a + 1] > '0')
-break;
-f = 0;
+	while (s[a])
+	{
+		if (s[a] == 45)
+		{
+			c *= -1;
+		}
+		while (s[a] >= 48 && s[a] <= 57)
+		{
+			d = 1;
+			b = (b * 10) + (s[a] - '0');
+			a++;
+		}
+		if (d == 1)
+		{
+			break;
+		}
+		a++;
+	}
+	b *= c;
+	return (b);
 }
-a++;
-}
-if (f == 0)
-return (0);
 
-return (c);
-}
