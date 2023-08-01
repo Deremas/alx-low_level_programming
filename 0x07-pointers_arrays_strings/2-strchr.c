96491835979549c2943734fa27f0b81b4b690f48
Dereje Masresha <deremas27@gmail.com>
2023-07-31 18:36:57 -0700

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* _strchr - function that locates a character in a string
+* @s: sring tobe searched
+* @c: character to be located
+*
+* Return: if c is found - a pointer to the first occurrence
+* if c is not found I NULL
+*/
+
+char *_strchr(char *s, char c)
+
+{
+	int index;
+
+	for (index = 0; s[index] >= '\0'; index++)
+	{
+		if (s[index] == c)
+			return (s + index);
+	}
+
+	return ('\0');
+}
