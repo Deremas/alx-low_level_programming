000492c45b058fd5d8c126a8537555aab2813322
Dereje Masresha <deremas27@gmail.com>
2023-08-16 19:39:52 -0700

    1-array_iterator.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * print_name - prints name
+ * @name: name to be printed
+ * @f: pointer function
+ * Return: 0
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL && f == NULL)
+		return;
+	f(name);
+}
