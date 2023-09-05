280a6a3e9e574dccc12bfd90332df8d67ec33a63
Deremas <deremas27@gmail.com>
2023-09-04 22:45:27 -0700

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -29,25 +29,25 @@
 
 int main(int argc, char **argv)
 {
-	int my_file_from, my_file_to, from_copy, to_copy, c1, c2;
+	int fd_from, fd_to, from_copy, to_copy, c1, c2;
 	char buffer[1024];
 
 	if (argc != 3)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 
-	my_file_from = open(argv[1], O_RDONLY);
-	if (my_file_from == -1)
+	fd_from = open(argv[1], O_RDONLY);
+	if (fd_from == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	my_file_to = open(argv[2], O_CREAT | O_WRONLY | O_APPEND | O_TRUNC, 0664);
-	if (my_file_to == -1)
+	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_APPEND | O_TRUNC, 0664);
+	if (fd_to == -1)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
-	
-	while ((from_copy = read(my_file_from, buffer, 1024)) > 0)
+	/* read a copy with a buffer size of 1024 */
+	while ((from_copy = read(fd_from, buffer, 1024)) > 0)
 	{
-		to_copy = write(my_file_to, buffer, from_copy);
+		to_copy = write(fd_to, buffer, from_copy);
 		if (from_copy != to_copy)
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 	}
@@ -56,12 +56,12 @@ int main(int argc, char **argv)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-
-	c1 = close(my_file_from);
+	/*clode fd */
+	c1 = close(fd_from);
 	if (c1 == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", my_file_from), exit(100);
-	c2 = close(my_file_to);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from), exit(100);
+	c2 = close(fd_to);
 	if (c2 == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", my_file_to), exit(100);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to), exit(100);
 	return (0);
 }
